1cdec01c4408c4619e08b6ef139a1f88a9940cbe
kimagin <info@kimagination.net>
2023-03-04 20:50:03 +0100

    Initial

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/* 📑 This is a pure JS app made for quick and dirty tasks
+      It can be run by: 
+      - nodemon main.js -q
+      - npm run dev
+
+   ⚡ No speciall features are not installed in this app and in case, anything will be necessary, it should be created manually. 
+*/
+
+// 🚩 JS Code comes here
